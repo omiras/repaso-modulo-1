7c97d4aa596d72ca9e5456beb48101b117915021
VanyaMark <markova.vanya@gmail.com>
2024-06-06 18:50:59 +0200

    remove rotten done

--- algoritmia/1-remove-rotten.js
+++ algoritmia/1-remove-rotten.js
@@ -2,11 +2,27 @@
 
 function removeRotten(bagOfFruits) {
     // ...
+    let freshFruits = [];
+    if (bagOfFruits.length < 1 || !bagOfFruits) {
+        return [];
+    }
+    else if (bagOfFruits.length > 0) {
+        bagOfFruits.forEach(fruit => {
+            if (fruit.includes('rotten') == false) {
+                freshFruits.push(fruit);
+            }
+            if (fruit.includes('rotten') == true) {
+                fruit = fruit.slice(6, fruit.length).toLowerCase();
+                freshFruits.push(fruit);
+                }
+                })
+                }
+            return freshFruits;
 }
 
 console.log(removeRotten([])); // []
 
 console.log(removeRotten(["apple", "banana", "kiwi", "melone", "orange"])); // ["apple","banana","kiwi","melone","orange"])
 
-console.log(removeRotten(["apple", "rottenBanana", "apple"])); //["apple","banana","apple"] 
+console.log(removeRotten(["apple", "rottenBanana", "apple", "rottenApple"])); //["apple","banana","apple"] 
 
